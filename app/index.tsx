01fe33fcbffc790724e067ffbeb87c1600484663
elsacodesid <elsacodesid@gmail.com>
2024-07-19 14:21:35 +0700

    update

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,14 @@
-import { View, Text, ImageBackground, SafeAreaView} from "react-native";
+import { View, Text, ImageBackground, SafeAreaView } from "react-native";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
-import {StatusBar} from "expo-status-bar"
+import { StatusBar } from "expo-status-bar";
 import beachImage from "@/assets/meditation-images/beach.webp";
 import CustomButton from "@/components/CustomButton";
+import { useRouter } from "expo-router";
+import AppGradient from "@/components/AppGradient";
 
 const App = () => {
+  const router = useRouter();
   return (
     <View className="flex-1">
       <ImageBackground
@@ -13,21 +16,23 @@ const App = () => {
         resizeMode="cover"
         className="flex-1"
       >
-        <LinearGradient
-          className="flex-1"
-          colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}
-        >
-          <SafeAreaView className="flex-1 mx-5 my-12 justify-between ">
+        <AppGradient colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}>
+          <SafeAreaView className="flex-1 px-1 justify-between ">
             <View>
               <Text className="text-center text-white font-bold text-4xl">
                 Simple Meditation
               </Text>
-              <Text className="text-center text-regular text-white text-2xl mt-3">Simplifying Meditation for Everyone</Text>
-            </View>     
+              <Text className="text-center text-regular text-white text-2xl mt-3">
+                Simplifying Meditation for Everyone
+              </Text>
+            </View>
             <StatusBar style="light" />
-            <CustomButton onPress={() => console.log("tap")} title="Get Started" />
+            <CustomButton
+              onPress={() => router.push("/nature-meditate")}
+              title="Get Started"
+            />
           </SafeAreaView>
-        </LinearGradient>
+        </AppGradient>
       </ImageBackground>
     </View>
   );
