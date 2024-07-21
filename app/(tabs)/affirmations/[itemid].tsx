5fc0721940383316a5a10270d4be37a260848b96
elsacodesid <elsacodesid@gmail.com>
2024-07-21 09:06:16 +0700

    update

--- app/(tabs)/affirmations/[itemid].tsx
+++ app/(tabs)/affirmations/[itemid].tsx
@@ -1,31 +1,72 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  ImageBackground,
+  Pressable,
+  ScrollView,
+} from "react-native";
 import React, { useEffect, useState } from "react";
-import { useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams, router } from "expo-router";
 import { GalleryPreviewData } from "@/constants/models/AffirmationCategory";
 import AFFIRMATION_GALLERY from "@/constants/affirmation-gallery";
+import AppGradient from "@/components/AppGradient";
+import { AntDesign } from "@expo/vector-icons";
 
 const AffirmationPractice = () => {
   const { itemid } = useLocalSearchParams();
 
-  const [affirmation, setAffirmation] = useState<GalleryPreviewData>()
+  const [affirmation, setAffirmation] = useState<GalleryPreviewData>();
+  const [sentences, setSentences] = useState<string[]>([])
   useEffect(() => {
- for ( let idx = 0; idx < AFFIRMATION_GALLERY.length; idx++){
-  const affirmationData = AFFIRMATION_GALLERY[idx].data
-  
-  const affirmationToStart = affirmationData.find((a) => a.id === Number(itemid) )
+    for (let idx = 0; idx < AFFIRMATION_GALLERY.length; idx++) {
+      const affirmationData = AFFIRMATION_GALLERY[idx].data;
 
-  if (affirmationToStart){
-    setAffirmation(affirmationToStart)
+      const affirmationToStart = affirmationData.find(
+        (a) => a.id === Number(itemid)
+      );
 
-    return
-  }
- }
-    
-  }, [])
-  
+      if (affirmationToStart) {
+        setAffirmation(affirmationToStart);
+        const affirmationArray = affirmationToStart.text.split(".")
+        //Remove the last element if it's an empty string
+
+        if (affirmationArray[affirmationArray.length -1] === "") {
+          affirmationArray.pop()
+        }
+
+        setSentences(affirmationArray)
+
+        return;
+      }
+    }
+  }, []);
+
   return (
-    <View>
-      <Text>AffirmationPractice</Text>
+    <View className="flex-1">
+      <ImageBackground
+        source={affirmation?.image}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <AppGradient colors={["rgba(0,0,0,0.3)", "rgba(0,0,0,0.9)"]}>
+          <Pressable
+            onPress={() => router.back()}
+            className="absolute top-16 left-6 z-10"
+          >
+            <AntDesign name="leftcircleo" size={50} color={"white"} />
+          </Pressable>
+          <ScrollView className="mt-20 " showsVerticalScrollIndicator={false}>
+            <View className="h-full justify-center">
+              <View className="h-4/5 justify-center">
+              {sentences.map((sentence, index) => (
+    <Text className="text-white text-2xl mb-12 mt-10 text-center">{sentence}.</Text>
+              ))}
+            
+              </View>
+            </View>
+          </ScrollView>
+        </AppGradient>
+      </ImageBackground>
     </View>
   );
 };
