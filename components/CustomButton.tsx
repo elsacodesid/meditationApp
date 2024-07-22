9c5caf6e67025367fdd0cc1d172114be0d72a78a
elsacodesid <elsacodesid@gmail.com>
2024-07-22 09:30:11 +0700

    update

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -17,7 +17,7 @@ const CustomButton = ({
   return (
     <TouchableOpacity activeOpacity={0.7}
     className={`bg-white rounded-xl min-h-[62px] justify-center items-center ${containerStyles}`} onPress={onPress}>
-      <Text className={`font-semibold text-lg ${textStyles}`}>Get Started</Text>
+      <Text className={`font-semibold text-lg ${textStyles}`}>{title}</Text>
     </TouchableOpacity>
   );
 };
